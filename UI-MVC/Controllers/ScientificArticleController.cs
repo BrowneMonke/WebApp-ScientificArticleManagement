bdaaf5fc7f0b5892ec6bc2420897827c98823dfa
Akshat Verma <akshat.verma@student.kdg.be>
2024-12-10 02:05:14 +0100

    Created ScientificArticleController

---
+++ UI-MVC/Controllers/ScientificArticleController.cs
@@ -0,0 +1,24 @@
+﻿using ArticleManagement.BL;
+using ArticleManagement.BL.Domain;
+using ArticleManagement.UI.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticleManagement.UI.Web.Controllers;
+
+public class ScientificArticleController
+{
+    private readonly IManager _manager;
+
+    public ScientificArticleController(IManager manager)
+    {
+        _manager = manager;
+    }
+    
+    public ViewResult Index()
+    {
+        var articles = _manager.GetAllArticles();
+        return View(articles);
+    }
+    
+}
