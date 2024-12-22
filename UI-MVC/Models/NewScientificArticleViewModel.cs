546c1b17ce00a355834b25f36de36d75196fab3f
Akshat Verma <akshat.verma@student.kdg.be>
2024-12-22 20:37:16 +0100

    Added feature - Add Article

--- UI-MVC/Models/NewScientificArticleViewModel.cs
+++ UI-MVC/Models/NewScientificArticleViewModel.cs
@@ -7,8 +7,9 @@ public class NewScientificArticleViewModel
 {
     [Required] [StringLength(256, MinimumLength = 5)]
     public string Title { get; init; }
-    [Required] [CustomValidation(typeof(NewScientificArticleViewModel), nameof(ValidateDateOfPublication))]
+    [Required(ErrorMessage = "The Date of Publication field is required.")] [CustomValidation(typeof(NewScientificArticleViewModel), nameof(ValidateDateOfPublication))]
     public DateOnly DateOfPublication { get; set; }
+    [Required(ErrorMessage = "The Number of Pages field is required.")]
     public int NumberOfPages { get; set; }
     public ArticleCategory Category { get; set; }
 
